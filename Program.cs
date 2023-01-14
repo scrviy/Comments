9f36c7a6c91df65bf0aeab4d24ea33005e5cad47
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-14 15:52:02 -0300

    anotacoes sobre comentarios

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Comments
+{
+    public class Comments
+    {
+        public static void Main(string[] args)
+        {
+            // Comentário em Csharp.
+            Console.WriteLine("Bem vindo ao módulo de comentários na linguagem c#.");
+        }
+    }
+}
